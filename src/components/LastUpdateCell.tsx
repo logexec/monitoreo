cbd1dda795dbc6e5b63e1b4036d37a38baf99599
rick-dev-logex <ricardo.estrella@logex.ec>
2025-03-10 16:38:20 -0500

    updated colors

--- src/components/LastUpdateCell.tsx
+++ src/components/LastUpdateCell.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { getTimeElapsed } from '../utils/timeUtils';
-import { TripUpdate } from '../types/database';
+import { getTimeElapsed } from "../utils/timeUtils";
+import { TripUpdate } from "../types/database";
 
 interface LastUpdateCellProps {
   updates: TripUpdate[];
@@ -9,10 +8,16 @@ interface LastUpdateCellProps {
 export function LastUpdateCell({ updates }: LastUpdateCellProps) {
   const lastUpdate = updates[0]?.created_at;
   const timeElapsed = getTimeElapsed(lastUpdate);
-  
+
   return (
     <div className="min-w-[140px] truncate">
-      <span className={`${lastUpdate ? 'text-gray-900' : 'text-gray-400'}`}>
+      <span
+        className={`${
+          lastUpdate
+            ? "text-gray-900 dark:text-gray-100"
+            : "text-gray-400 dark:text-gray-600"
+        }`}
+      >
         {timeElapsed}
       </span>
     </div>
