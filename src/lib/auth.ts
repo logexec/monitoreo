8552e37d11ee73596d5a06daf917846d0b1f61ba
rick-dev-logex <ricardo.estrella@logex.ec>
2025-03-26 09:59:28 -0500

    updated auth.ts file

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,9 @@
-// src/auth.ts
 import axios from "./axios";
 import { toast } from "sonner";
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 export async function getUser() {
   try {
-    const response = await axios.get(`${API_URL}/me`); // Usa la URL base configurada
+    const response = await axios.get(`/me`); // Usa la URL base configurada
     return response.data;
   } catch (error) {
     toast.error(
