5ef793f57c41e88b2bf9e934cf9e232ad4df601b
rick-dev-logex <ricardo.estrella@logex.ec>
2025-04-11 16:35:44 -0500

    updated dark mode

--- src/components/Users/CopyToClipboardAlert.tsx
+++ src/components/Users/CopyToClipboardAlert.tsx
@@ -16,21 +16,23 @@ const CopyToClipboardAlert = () => {
   };
 
   return (
-    <Alert className="py-4 px-2 rounded-md bg-sky-50 border border-sky-200">
+    <Alert className="py-4 px-2 rounded-md bg-sky-50 dark:bg-sky-950 border border-sky-200 dark:border-sky-800">
       <div className="flex items-center space-x-2">
-        <Info className="h-5 w-5 text-sky-600" />
-        <AlertTitle className="text-sky-800">Recuerda</AlertTitle>
+        <Info className="h-5 w-5 text-sky-600 dark:text-sky-400" />
+        <AlertTitle className="text-sky-800 dark:text-sky-200">
+          Recuerda
+        </AlertTitle>
       </div>
       <AlertDescription className="mt-1.5 flex items-center justify-center space-x-2">
-        <span className="text-sm text-gray-700">
+        <span className="text-sm text-gray-700 dark:text-gray-300">
           La contraseña por defecto es
         </span>
-        <div className="relative border rounded border-sky-200 px-0.5 py-1 w-40">
+        <div className="relative border rounded border-sky-200 dark:border-sky-800 px-0.5 py-1 w-40">
           <input
             type="text"
             value={password}
             readOnly
-            className="bg-transparent focus:outline-none cursor-pointer text-sm text-sky-600 pl-4"
+            className="bg-transparent focus:outline-none cursor-pointer text-sm text-sky-600 dark:text-sky-400 pl-4"
           />
           <button
             onClick={copyToClipboard}
