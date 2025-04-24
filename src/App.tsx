117acd32ce4c5284f6f8c8bf2ca182806be1df51
rick-dev-logex <ricardo.estrella@logex.ec>
2025-04-24 12:24:53 -0500

    removed overflow x

--- src/App.tsx
+++ src/App.tsx
@@ -13,42 +13,45 @@ import UsersPage from "./pages/UsersPage";
 import GPSHistoryPage from "./pages/GPSHistoryPage";
 import { AuthProvider } from "./contexts/AuthContext";
 import Dashboard from "./pages/Dashboard";
+import { GlobalFilterProvider } from "./contexts/DashboardFilterContext";
 
 export function App() {
   return (
     <Router>
       <AuthProvider>
-        <Toaster position="top-center" duration={4500} richColors={true} />
-        <div className="min-h-screen">
-          <ToasterToast position="top-right" />
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route element={<ProtectedRoute />}>
-              <Route
-                path="/"
-                element={
-                  <RequireAuth>
-                    <Sidebar />
-                  </RequireAuth>
-                }
-              >
-                <Route path="/" element={<Dashboard />} index />
-                <Route path="/dashboard" element={<Dashboard />} />
-                {/* Administración */}
-                <Route path="/users" element={<UsersPage />} />
+        <GlobalFilterProvider>
+          <Toaster position="top-center" duration={4500} richColors={true} />
+          <div className="min-h-screen">
+            <ToasterToast position="top-right" />
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route element={<ProtectedRoute />}>
+                <Route
+                  path="/"
+                  element={
+                    <RequireAuth>
+                      <Sidebar />
+                    </RequireAuth>
+                  }
+                >
+                  <Route path="/" element={<Dashboard />} index />
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  {/* Administración */}
+                  <Route path="/users" element={<UsersPage />} />
 
-                {/* Viajes */}
-                <Route path="/trips" element={<TripList />} />
-                <Route path="/new-trip" element={<AddPage />} />
-                <Route path="/upload" element={<UploadPage />} />
-                <Route path="/updates" element={<UpdatesPage />} />
+                  {/* Viajes */}
+                  <Route path="/trips" element={<TripList />} />
+                  <Route path="/new-trip" element={<AddPage />} />
+                  <Route path="/upload" element={<UploadPage />} />
+                  <Route path="/updates" element={<UpdatesPage />} />
 
-                {/* GPS */}
-                <Route path="/gps-history" element={<GPSHistoryPage />} />
+                  {/* GPS */}
+                  <Route path="/gps-history" element={<GPSHistoryPage />} />
+                </Route>
               </Route>
-            </Route>
-          </Routes>
-        </div>
+            </Routes>
+          </div>
+        </GlobalFilterProvider>
       </AuthProvider>
     </Router>
   );
